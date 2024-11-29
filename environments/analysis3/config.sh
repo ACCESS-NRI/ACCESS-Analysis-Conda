cb771f68ed6b40fa385bcab477ddee0fb209fbe6
rbeucher <romain.beucher@anu.edu.au>
2024-11-29 12:19:55 +1000

    Try build with openmpi7 from Gadi

--- environments/analysis3/config.sh
+++ environments/analysis3/config.sh
@@ -8,16 +8,14 @@
 ### outside_files_to_copy
 
 ### Optional config for custom deploy script
-export VERSION_TO_MODIFY=24.12
-export STABLE_VERSION=24.04
-export UNSTABLE_VERSION=24.12
+export VERSION_TO_MODIFY=24.11
+export STABLE_VERSION=24.11
+export UNSTABLE_VERSION=24.11
 
 ### Version settings
 export ENVIRONMENT=analysis3
 export FULLENV="${ENVIRONMENT}-${VERSION_TO_MODIFY}"
 
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
-declare -a replace_from_apps=( "ucx/1.15.0" )
+declare -a replace_from_apps=( "ucx/1.15.0" "openmpi/4.1.7" )
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-declare -a outside_files_to_copy=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
-declare -a replace_with_external=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
