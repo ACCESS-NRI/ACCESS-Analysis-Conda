5afbfab690e7b746aaa936c45b5561d1a8d6b884
rbeucher <romain.beucher@anu.edu.au>
2025-02-07 09:38:36 +1000

    Analysis3-25.02-edge

--- environments/analysis3/config.sh
+++ environments/analysis3/config.sh
@@ -8,16 +8,16 @@
 ### outside_files_to_copy
 
 ### Optional config for custom deploy script
-export VERSION_TO_MODIFY=25.02
+export VERSION_TO_MODIFY=25.02e
 export STABLE_VERSION=24.12
-export UNSTABLE_VERSION=25.02
+export UNSTABLE_VERSION=25.02e
 
 ### Version settings
 export ENVIRONMENT=analysis3
 export FULLENV="${ENVIRONMENT}-${VERSION_TO_MODIFY}"
 
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
-declare -a replace_from_apps=( "ucx/1.15.0" )
+declare -a replace_from_apps=( "ucx/1.15.0" openmpi/5.9.5)
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-declare -a outside_files_to_copy=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
-declare -a replace_with_external=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
+#declare -a outside_files_to_copy=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
+#declare -a replace_with_external=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
