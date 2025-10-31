8644d9d29894b98fa4116799832b9473d38bc06c
Romain Beucher <romain.beucher@anu.edu.au>
2025-10-31 15:21:18 +1000

    Update environment configuration for analysis3-edge to version 25.11 (#337)

--- environments/analysis3-edge/config.sh
+++ environments/analysis3-edge/config.sh
@@ -8,9 +8,9 @@
 ### outside_files_to_copy
 
 ### Optional config for custom deploy script
-export VERSION_TO_MODIFY=25.10
-export STABLE_VERSION=25.09
-export UNSTABLE_VERSION=25.10
+export VERSION_TO_MODIFY=25.11
+export STABLE_VERSION=25.10
+export UNSTABLE_VERSION=25.11
 
 ### Version settings
 export ENVIRONMENT=analysis3_edge
@@ -22,5 +22,3 @@ export CONDA_OVERRIDE_CUDA=12
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
 declare -a replace_from_apps=( "ucx/1.15.0" openmpi/5.9.5)
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-#declare -a outside_files_to_copy=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
-#declare -a replace_with_external=( "/g/data/xp65/public/apps/openmpi/4.1.6" )
