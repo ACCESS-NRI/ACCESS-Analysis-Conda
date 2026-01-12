786727d005ad62b8d874e490cf3854a1ea89d101
Sean Bryan <sean.bryan@anu.edu.au>
2026-01-12 14:58:59 +1100

    Update benchcab to 4.3.1
    The OPAL_PREFIX environment variable introduced in PR #352 causes the
    xp65 MPI library to take precedence over any MPI libraries loaded via
    module load - as a workaround, this change unsets this variable from the
    build environment to fix this issue.
    
    References https://github.com/CABLE-LSM/benchcab/pull/341

--- environments/benchcab/config.sh
+++ environments/benchcab/config.sh
@@ -1,6 +1,6 @@
 # Note: BENCHCAB_VERSION should match the version of benchcab in
 # environments/benchcab/environment.yml:
-BENCHCAB_VERSION=4.3.0
+BENCHCAB_VERSION=4.3.1
 export ENVIRONMENT="benchcab"
 export STABLE_VERSION="${BENCHCAB_VERSION}"
 export FULLENV="${ENVIRONMENT}-${BENCHCAB_VERSION}"
