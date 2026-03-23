c12a2e6cf93e5b6183e7fe5ddbcccd9022054356
Charles Turner <charles.turner@anu.edu.au>
2026-03-23 13:26:38 +0800

    Input free bash script to handle updating environment.yml

---
+++ environments/analysis3/recreate-env-yml.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+set -e 
+
+mv environment.yml environment.yml.bak
+(
+echo "name: analysis3"
+echo "channels:"
+echo "  - accessnri"
+echo "  - conda-forge"
+echo "  - nodefaults"
+echo "  - rapidsai"
+echo "  - pytorch"
+echo "  - nvidia"
+echo "dependencies:"
+
+# Conda packages
+jq -r '
+.[] 
+| select(.name | startswith("__") | not)
+| select(.name | startswith("_") | not)
+| select(.build != null)
+| "  - \(.name)=\(.version)=\(.build)"
+' solved.json
+
+echo "  - pip"
+echo "  - pip:"
+
+# Pip packages
+jq -r '
+.[] 
+| select(.name | startswith("__") | not)
+| select(.name | startswith("_") | not)
+| select(.build == null)
+| "    - \(.name | gsub("_"; "-"))==\(.version)"
+' solved.json
+
+) > environment.yml
